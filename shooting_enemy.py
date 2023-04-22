d1bbb32dcad198948641c78e4e07d12fd9e36173
Moonhee Lee <moonhee.lee@fourseasonsoft.com>
2023-04-21 19:41:02 -0700

    refactor ShootingEnemy

--- shooting_enemy.py
+++ shooting_enemy.py
@@ -4,27 +4,27 @@ from enemy_bullet import EnemyBullet
 
 
 class ShootingEnemy(pygame.sprite.Sprite):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, screen_width, screen_height, target, bullet_group):
         super().__init__()
         self.image = pygame.Surface((50, 50))
         self.image.fill((255, 255, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, args[0] - self.rect.width)
+        self.rect.x = random.randint(0, screen_width - self.rect.width)
         self.rect.y = -self.rect.height
-        self.height = args[1]
+        self.screen_height = screen_height
         self.last_shot_time = pygame.time.get_ticks()
         self.shot_interval = random.randint(500, 900)  # in milliseconds
-        self.target = kwargs['target']
-        self.bullet_group = kwargs['bullet_group']
+        self.target = target
+        self.bullet_group = bullet_group
         self.fired = False
 
     def update(self):
         self.rect.y += 5
-        if self.rect.y > self.height:
+        if self.rect.y > self.screen_height:
             self.kill()
 
-        if self.fired is False and pygame.time.get_ticks() - self.last_shot_time > self.shot_interval:
+        if not self.fired and pygame.time.get_ticks() - self.last_shot_time > self.shot_interval:
             self.last_shot_time = pygame.time.get_ticks()
-            bullet = EnemyBullet(self.rect.centerx, self.rect.bottom, self.height, [self.target.rect.x, self.target.rect.y])
+            bullet = EnemyBullet(self.rect.centerx, self.rect.bottom, self.screen_height, [self.target.rect.x, self.target.rect.y])
             self.bullet_group.add(bullet)
             self.fired = True
